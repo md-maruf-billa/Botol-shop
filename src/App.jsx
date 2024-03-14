047cb51356ae0b069ad276eba8a3b717ae4d2839
eng-maruf <142441055+eng-maruf@users.noreply.github.com>
2024-03-15 00:38:18 +0600

    Added react tost

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,44 @@ import { useState } from 'react'
 import './App.css'
 import Main from './components/body/main'
 import { Header } from './components/header/header'
-Header
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   const [clickCart, setClickCard] = useState([])
 
+  const warning = () => {
+    toast.warn('You Already Added this product',{
+      position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+  }
 
   const cartClickHandle = (product) => {
-    const isExist = clickCart.find(pd=>pd.id == product.id)
+    const isExist = clickCart.find(pd => pd.id == product.id)
     console.log(isExist)
-    if(isExist){
-      alert('pagla')
+    if (isExist) {
+      warning();
+      return;
     }
-    else{
-      setClickCard([...clickCart,product])    
+    else {
+      toast.success('🦄 Wow so easy!', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      setClickCard([...clickCart, product])
     }
   }
   return (
@@ -28,6 +53,7 @@ function App() {
         cartClickHandle={cartClickHandle}
         clickCart={clickCart}
       ></Main>
+      <ToastContainer />
     </>
   )
 }
